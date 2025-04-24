6871f7c5f02a8b04f078f2c7bcbd29cfd2246c46
adlt456 <adrian.delatorre1@hotmail.com>
2025-04-23 22:26:45 -0700

    replace ino file with main.cpp
    better supported by platform io.

---
+++ Firmware/250423-162135-esp32dev/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include "led.h"
+
+// Function prototypes (forward declarations)
+void Task1(void *pvParameters);
+void Task2(void *pvParameters);
+
+void setup() {
+  Serial.begin(115200);
+  setupToggleLed(5);
+
+  // Create Task 1 and Task 2
+  xTaskCreate(Task1, "Task1", 10000, NULL, 1, NULL);
+  xTaskCreate(Task2, "Task2", 10000, NULL, 1, NULL);
+}
+
+void loop() {
+  // Empty loop as tasks are running in the background
+}
+
+void Task1(void *pvParameters) {
+  while (true) {
+    Serial.println("Task 1 is running");
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+  }
+}
+
+void Task2(void *pvParameters) {
+  while (true) {
+    Serial.println("Task 2 is running. LED toggle");
+    toggleLed();
+    vTaskDelay(2000 / portTICK_PERIOD_MS); // Delay for 2 seconds
+  }
+}
